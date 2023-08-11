957674cdb8cd358b06f6b290ac3b481167bfb329
isasumer <sumerisa5@gmail.com>
2023-08-11 10:49:37 +0300

    I have installed firebase

--- pages/firebase.js
+++ pages/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCYmkUwmz7wiz3zL4B5ijSzncUJ8deO-0",
@@ -11,5 +11,5 @@ const firebaseConfig = {
   measurementId: "G-359NMPG03H"
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
